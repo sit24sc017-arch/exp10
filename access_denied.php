35a06dc5f45276d6984ebc17a3fa4cfb87065fe4
sit24sc017-arch <sit24sc017@sairamtap.edu.in>
2026-08-13 13:54:41 +0530

    Create access_denied.php

---
+++ access_denied.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Access Denied</title>
+</head>
+<body>
+
+<h2>Access Denied</h2>
+
+<p>You do not have permission to access this page.</p>
+
+<a href="login.php">Go to Login Page</a>
+
+</body>
+</html>
