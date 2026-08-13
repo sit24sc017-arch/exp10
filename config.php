42f5a2fb91a1f84a156eea63cca4944cc6c85f5e
sit24sc017-arch <sit24sc017@sairamtap.edu.in>
2026-08-13 13:52:24 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "rbac_system";
+
+// Create Connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check Connection
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+?>
